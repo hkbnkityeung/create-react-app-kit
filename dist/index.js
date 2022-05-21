8082c80bc14d394313084c524ddf51cb6291a99d
Kit Yeung <kit.yeung@cs.hkbn.com.hk>
2022-05-21 21:04:58 +0800

    first commit

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+"use strict";
+function init(log) {
+    console.log(log);
+}
+init('Hello, Create-React-App-Kit!');
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+function init(log: string) {
+    console.log(log)
+}
+
+init('Hello, Create-React-App-Kit!')
